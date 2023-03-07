f1beb0892c02372a6f87188c4104af77e46e27f2
Florent Clarret <florent.clarret@datadoghq.com>
2023-03-07 10:23:42 +0100

    Fix the e2e env start command

---
+++ kafka_consumer/tests/docker/kerberos/kdc/start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Kafka service principal:
+kadmin.local -w password -q "add_principal -randkey kafka/broker.kerberos-demo.local@TEST.CONFLUENT.IO"  > /dev/null
+
+# Zookeeper service principal:
+kadmin.local -w password -q "add_principal -randkey zookeeper/zookeeper.kerberos-demo.local@TEST.CONFLUENT.IO"  > /dev/null
+
+# Create a principal with which to connect to Zookeeper from brokers - NB use the same credential on all brokers!
+kadmin.local -w password -q "add_principal -randkey zkclient@TEST.CONFLUENT.IO"  > /dev/null
+
+# Create client principals to connect in to the cluster:
+kadmin.local -w password -q "add_principal -randkey kafka/localhost@TEST.CONFLUENT.IO"  > /dev/null
+
+kadmin.local -w password -q "ktadd  -k /var/lib/secret/zookeeper.key -norandkey zookeeper/zookeeper.kerberos-demo.local@TEST.CONFLUENT.IO " > /dev/null
+kadmin.local -w password -q "ktadd  -k /var/lib/secret/zookeeper-client.key -norandkey zkclient@TEST.CONFLUENT.IO " > /dev/null
+kadmin.local -w password -q "ktadd  -k /var/lib/secret/localhost.key -norandkey kafka/localhost@TEST.CONFLUENT.IO " > /dev/null
+
+chmod a+r /var/lib/secret/broker.key
+chmod a+r /var/lib/secret/zookeeper.key
+chmod a+r /var/lib/secret/zookeeper-client.key
+chmod a+r /var/lib/secret/localhost.key
+
+/usr/sbin/krb5kdc -n
